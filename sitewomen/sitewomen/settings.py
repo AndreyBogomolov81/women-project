8605b92b3b2c0839cb6ad386b2233ded4b0766ca
Андрей Богомолов <andrey8110@mail.ru>
2023-11-07 20:57:57 +0300

    End firstlesson 1.4

--- sitewomen/sitewomen/settings.py
+++ sitewomen/sitewomen/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sitewomen.women.apps.WomenConfig'
+    'women.apps.WomenConfig'
 ]
 
 MIDDLEWARE = [
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('<h1>Main page</h1>')
+
+def categories(request):
+    return HttpResponse('<h1>Categories</h1>')
