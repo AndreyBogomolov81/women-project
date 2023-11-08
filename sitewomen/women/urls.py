45e8067caaecdea929217fc93e75a5c3e4b54a81
Андрей Богомолов <andrey8110@mail.ru>
2023-11-08 20:36:28 +0300

    studied dynamic URL

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 
-from women import views
+from women import views, converters
+
+register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path('', views.index),
-    path('cats/', views.categories)
+    path('cats/<int:cat_id>/', views.categories),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),
+    path('cats/posts/<int:cat_id>/<slug:cat_slug>/', views.categories_by_double),
+    path('archive/<year4:year>/', views.categories_by_re),
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -6,5 +6,15 @@ from django.shortcuts import render
 def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse('<h1>Main page</h1>')
 
-def categories(request):
-    return HttpResponse('<h1>Categories</h1>')
+def categories(request, cat_id):
+    return HttpResponse(f'<h1>Categories</h1><p>Cat id: {cat_id}')
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f'<h1>Categories</h1><p>Cat slug: {cat_slug}')
+
+
+def categories_by_re(request, year):
+    return HttpResponse(f'<h1>Categories</h1><p>Year: {year}')
+
+def categories_by_double(requests, cat_id, cat_slug):
+    return HttpResponse(f'<h1>Categories by double</h1><p>Cat_id: {cat_id} Cat_slug: {cat_slug}')
