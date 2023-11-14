b1078774ca4915eb8b5e056cddef5a06974395e2
Andrey <ab0836315@gmail.com>
2023-11-14 21:00:20 +0300

    studied 3.3 lesson

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Women(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ShopItem(models.Model):
+    name = models.CharField(max_length=100)
+    weight = models.IntegerField(default=0, blank=True)
+    price = models.IntegerField(default=0)
+    is_exists = models.BooleanField(default=True)
